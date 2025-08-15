55a14d9307393b2653644246df5776fd0fd95f2a
unknown <lab3131@etecjk.local>
2025-08-14 22:20:19 -0300

    Exercicio - Tabuada com matrizes

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ console.log(array[10 - 8]); //acessa uma posição por uma operação matematica
 // array[5] = 152.1
 // console.log(array)
 
+/*
 let matriz = [
     [1,2,3,4,5],
     [6,7,8,9,10],
@@ -70,3 +71,18 @@ let matriz = [
 ]
 
 console.log(matriz[2][2]) //Acessa o valor 13 dentro da matriz
+*/
+
+//Exercicio - Calcular a tabuada do 1 ao 10 usando uma matriz. Lembre-se uma matriz é um array dentro do outro.
+
+for(let col = 0; col <= 10; col++){
+    for(let linha = 0; linha <= 10; linha++){
+        let tabuada = col * linha
+        let matriz = [tabuada]
+        let array = [
+            [col]
+            [linha]
+        ]
+        console.log(array)
+    }
+}
